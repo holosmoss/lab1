195b528a1e3f216309627cb446201aecb8856be9
aimeric <holosmoss@gmail.com>
2015-09-14 12:18:08 -0400

    readAllBytes

--- lab1/src/lab1/main.java
+++ lab1/src/lab1/main.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class main {
 	//branche aimeric
@@ -14,14 +16,15 @@ public class main {
 	      System.out.print("Enter String : ");
 	      //ouvrir le fichier positioner dans le dossier du projet avec le nom indiquer dans la console
 	      String s = br.readLine();
-	      System.out.print("opening :"+s);
 	      File file = new File(s);
-	      FileReader fr = new FileReader(file);
-	      //TODO le faire sans set size
-	      char [] a = new char[50];
-	      fr.read(a); // reads the content to the array
-	      for(char c : a)
-	          System.out.print(c); //prints the characters one by one
-	      fr.close();
+	      Path path = file.toPath();
+	      byte [] b = Files.readAllBytes(path);
+//	      FileReader fr = new FileReader(file);
+//	      //TODO le faire sans set size
+//	      char [] a = new char[50];
+//	      fr.read(b); // reads the content to the array
+	      for(byte c : b)
+	          System.out.println(c); //prints the characters one by one
+//	      fr.close();
 	}
 }
